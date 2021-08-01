87120d6ca503b2326550cc66222dd5443e22fc45
Yuki Goto <ygoto@sun21.co.jp>
2021-08-01 23:33:50 +0900

    feat #25_Alignサンプルコード追加

---
+++ lib/views/widget-of-the-week/positioned.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lean/components/components.dart';
+
+class WidgetPositioned extends StatefulWidget {
+  @override
+  _WidgetPositionedState createState() => _WidgetPositionedState();
+}
+
+class _WidgetPositionedState extends State<WidgetPositioned>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Positioned"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            explanationRow(
+        Stack(
+          children: [
+            Positioned(
+              top: 25,left: 50,
+              width: 50,height: 70,
+              child: Container(
+                color: Colors.amber,
+                child: image(),))
+          ],
+        )
+        , "縦横の位置とサイズを調整することができる"),
+          explanationRow(
+        Stack(
+          children: [
+            Positioned.fill(
+              child: Container(
+                color: Colors.amber,
+                child: image(),))
+          ],
+        )
+        , "fillを使用すると位置やサイズの指定をせず、スペースを埋めることができる"),
+          ],
+        )
+        );
+  }
+}
