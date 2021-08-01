bd82a36f602fb76f551056277752849764abf7a7
Yuki Goto <ygoto@sun21.co.jp>
2021-08-01 23:01:06 +0900

    feat #24_BackdropFilterサンプルコード追加

---
+++ lib/views/widget-of-the-week/backdrop_filter.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:lean/components/components.dart';
+
+class WidgetBackdropFilter extends StatefulWidget {
+  @override
+  _WidgetBackdropFilterState createState() => _WidgetBackdropFilterState();
+}
+
+class _WidgetBackdropFilterState extends State<WidgetBackdropFilter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BackdropFilter"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          explanationRow(
+              Stack(
+                children: [
+                  image(),
+                  Positioned.fill(
+                      child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 1, sigmaY: 10),
+                          child: Container(
+                            // color: Colors.black.withOpacity(0),
+                            child: image(),
+                          ))),
+                ],
+              ),
+              "ImageFilter.blur:ぼかしフィルタを作成。（子はフィルタされない）"),
+        ],
+      ),
+    );
+  }
+}
