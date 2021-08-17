b0ad7c6d845891fb20c40cb43a401846cb8c54c2
Yuki Goto <ygoto@sun21.co.jp>
2021-08-17 22:52:32 +0900

    feat #99_Package__connectivityサンプルコード追加

---
+++ lib/views/widget-of-the-week/package__connectivity.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lean/components/components.dart';
+
+class WidgetPackage__connectivity extends StatefulWidget {
+  @override
+  _WidgetPackage__connectivityState createState() =>
+      _WidgetPackage__connectivityState();
+}
+
+class _WidgetPackage__connectivityState
+    extends State<WidgetPackage__connectivity> {
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Package__connectivity"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          explanationText("通信接続状態を確認することができる。"),
+          Center(child: Text('Connection Status: $_connectionStatus'))
+        ],
+      ),
+    );
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    ConnectivityResult result = ConnectivityResult.none;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    switch (result) {
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.mobile:
+      case ConnectivityResult.none:
+        setState(() => _connectionStatus = result.toString());
+        break;
+      default:
+        setState(() => _connectionStatus = 'Failed to get connectivity.');
+        break;
+    }
+  }
+}
+// https://pub.dev/packages/connectivity
